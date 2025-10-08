f2d4d9aff541933517294f1c96fb873e237fc487
Batsi97 <proanosebastian6@gmail.com>
2025-10-08 14:24:44 -0500

    Creacion de table afuera de la clase en cada entity

--- src/main/java/entities/Apunte.java
+++ src/main/java/entities/Apunte.java
@@ -1,12 +1,16 @@
 package entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-
-
+@Entity
+@Table(name="apuntes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Apunte
 {
     @Id
--- src/main/java/entities/Unidad.java
+++ src/main/java/entities/Unidad.java
@@ -1,9 +1,15 @@
 package entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="Unidades")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Unidad
 {
     @Id
