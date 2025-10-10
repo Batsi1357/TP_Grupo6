c3d883acd2ffddd4c9e98394c56774cb9ff1646a
Batsi97 <proanosebastian6@gmail.com>
2025-10-10 00:05:20 -0500

    En respuestasServiceImpl agregue datos vacios para la implementacion de la logica

--- src/main/java/serviceImpls/RespuestaServiceImpl.java
+++ src/main/java/serviceImpls/RespuestaServiceImpl.java
@@ -1,4 +1,39 @@
 package serviceImpls;
 
-public class RespuestaServiceImpl {
+import entities.Respuesta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.RespuestaRepository;
+import services.RespuestaService;
+
+import java.util.List;
+@Service
+public class RespuestaServiceImpl implements RespuestaService
+{
+    @Autowired
+    private RespuestaRepository respuestaRepository;
+    @Override
+    public List<Respuesta> list() {
+        return List.of();
+    }
+
+    @Override
+    public void insert(Respuesta respuesta) {
+
+    }
+
+    @Override
+    public Respuesta listId(int id) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void update(Respuesta respuesta) {
+
+    }
 }
