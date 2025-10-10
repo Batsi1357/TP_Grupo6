3a0ec31f8552d239d95e106c0bbc4839fd9f2343
Batsi97 <proanosebastian6@gmail.com>
2025-10-09 23:26:24 -0500

    En ServiceImpl agregue su metodos a OrderSubscripcion

--- src/main/java/serviceImpls/OrdenSubscripcionServiceImpl.java
+++ src/main/java/serviceImpls/OrdenSubscripcionServiceImpl.java
@@ -1,4 +1,53 @@
 package serviceImpls;
 
-public class OrdenSubscripcionServiceImpl {
+import entities.OrdenSubscripcion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.OrdenSubscripcionRepository;
+import repositories.SubscripcionRepository;
+import services.OrdenSubscripcionService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class OrdenSubscripcionServiceImpl implements OrdenSubscripcionService
+{
+    @Autowired
+    private OrdenSubscripcionRepository ordenSubscripcionRepository;
+
+    @Override
+    public List<OrdenSubscripcion> list() {
+        return List.of();
+    }
+
+    @Override
+    public void insert(OrdenSubscripcion ordenSubscripcion) {
+
+    }
+
+    @Override
+    public OrdenSubscripcion buscar(int id) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void update(OrdenSubscripcion ordenSubscripcion) {
+
+    }
+
+    @Override
+    public List<OrdenSubscripcion> listarPorRangoDeFechas(LocalDate FechaInicio, LocalDate FechaFin) {
+        return List.of();
+    }
+
+    @Override
+    public List<OrdenSubscripcion> listarPorCliente(int idCliente) {
+        return List.of();
+    }
 }
