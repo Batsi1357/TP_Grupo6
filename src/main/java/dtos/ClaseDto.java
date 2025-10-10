e975694107b71f83fb48df35403013e23bf24014
brandon <brandon@gmail.com>
2025-10-10 15:59:50 -0500

    Se ha completado en  Clase dto y Unidad dto

--- src/main/java/dtos/ClaseDto.java
+++ src/main/java/dtos/ClaseDto.java
@@ -1,4 +1,20 @@
 package dtos;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClaseDto {
+
+    private Integer id;
+    private Integer unidadId;
+    private String unidadTitulo;
+    private Boolean clasesPersonalizadas;
+
+
+
 }
--- src/main/java/dtos/UnidadDto.java
+++ src/main/java/dtos/UnidadDto.java
@@ -1,4 +1,20 @@
 package dtos;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UnidadDto {
+    private Integer id;
+    private String titulo;
+    private String descripcion;
+    private String nivel;
+    private String categoria;
+    private Integer duracion;
+    private Boolean estado;
+
 }
